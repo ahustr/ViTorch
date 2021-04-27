0d28eeed51eb40fd12353f57ed76216f8e36209f
unknown <49073682+PiKaChu-wcg@users.noreply.github.com>
2021-04-27 11:24:09 +0800

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Author: PiKaChu-wcg
+Github: https://github.com/PiKaChu-wcg/
+Date: 2021-04-26 15:11:05
+LastEditTime: 2021-04-26 15:11:05
+LastEditors: PiKaChu-wcg
+FilePath: \ViTorch\main.py
+Description: 
+'''
